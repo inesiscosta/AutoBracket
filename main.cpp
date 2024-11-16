b466402033e9fc069450a19f4d0f8eb0a8a67694
Inês Costa <94355111+inesiscosta@users.noreply.github.com>
2024-11-16 19:36:15 +0000

    Lastest changes main.cpp

--- main.cpp
+++ main.cpp
@@ -1,8 +1,6 @@
 #include <iostream>
-#include <vector>
 #include <string>
 #include <vector>
-#include <string>
 
 int main() {
   int _N, _M, desiredResult;
@@ -40,14 +38,14 @@ int main() {
   bool found = false;
 
   int len_sequence = sequence.size();
-  std::vector<std::vector<int>> dp(len_sequence, std::vector<int>(len_sequence, -1)); // Dynamic programming table
+  std::vector<std::vector<int>> dp(len_sequence,std::vector<int>(len_sequence, -1));  // Dynamic programming table
   std::vector<std::vector<std::string>> parentizations(len_sequence, std::vector<std::string>(len_sequence));
 
   for (int i = 0; i < len_sequence; ++i) {
     dp[i][i] = sequence[i];
     parentizations[i][i] = std::to_string(sequence[i]);
   }
-  
+
   for (int len = 2; len <= len_sequence; ++len) {
     for (int i = 0; i <= len_sequence - len; ++i) {
       int j = i + len - 1;
@@ -55,7 +53,7 @@ int main() {
         int left = dp[i][k];
         int right = dp[k + 1][j];
         if (left != -1 && right != -1) {
-          int result = operationTable[left-1][right-1];
+          int result = operationTable[left - 1][right - 1];
           dp[i][j] = result;
           parentizations[i][j] = "(" + parentizations[i][k] + " " + parentizations[k + 1][j] + ")";
           if (result == desiredResult && len == len_sequence) {
@@ -69,11 +67,11 @@ int main() {
 
   //---------------------------------------------------------------
   /* END FUNCTION parentization   */
-  
+
   // Print found result and parenthisised expression
   if (found) {
-      std::cout << "1" << std::endl << parentizedSequence << std::endl;
-      return 0;
+    std::cout << "1" << std::endl << parentizedSequence << std::endl;
+    return 0;
   }
   std::cout << "0" << std::endl;
   return 1;
