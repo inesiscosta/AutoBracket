b10324ec1a61e7741e08bfb5f5a30e4631e64d7b
Inês Costa <94355111+inesiscosta@users.noreply.github.com>
2024-12-03 21:13:17 +0000

    Small optimizations.

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,7 @@
 #include <tuple>
 
 std::vector<int> sequence;
+// dp[i][j] = {result, k, leftResult, rightResult}
 std::vector<std::vector<std::vector<std::tuple<int, int, int, int>>>> dp;
 
 std::string reconstruct(int i, int j, int result) {
@@ -25,6 +26,8 @@ int main() {
   // IOStream performance flags so it is closer to using stdio functions (scanf and printf)
   std::ios::sync_with_stdio(0);
   std::cin.tie(0);
+  
+  bool foundSolution = false;
 
   // n - matrix size; m - sequence length
   int n, m;
@@ -32,12 +35,11 @@ int main() {
 
   // Reads the matrix from stdin and adjusts the values to 0-Base Index
   std::vector<std::vector<int>> operatorTable(n, std::vector<int>(n));
-  for (int i = 0; i < n; ++i)
-    for (int j = 0; j < n; ++j)
-      std::cin >> operatorTable[i][j], --operatorTable[i][j];
+  for (std::vector<int> &row : operatorTable)
+    for (int &col : row)
+      std::cin >> col, --col;
 
   sequence.resize(m);
-  // dp[i][j] = {result, k, leftResult, rightResult}
   dp.resize(m, std::vector<std::vector<std::tuple<int, int, int, int>>>(m));
 
   // DP initialization
@@ -50,7 +52,12 @@ int main() {
   // Reads the target result from stdin and adjusts the value to 0-Base Index
   std::cin >> targetResult, --targetResult;
 
-  for (int len = 2; len <= m; ++len)
+  if (sequence[0] == targetResult && m == 1) {
+    std::cout << 1 << std::endl << sequence[0] + 1 << std::endl; // 1-Base Index adjustment for output
+    return 0;
+  }
+
+  for (int len = 2; len <= m; ++len) {
     // Iterate over all possible starting points of subexpressions
     for (int i = 0; i <= m - len; ++i) {
       int j = i + len - 1; // Ending point of the current subexpression
@@ -58,7 +65,7 @@ int main() {
       std::vector<bool> used(n, false);
       int resultsFound = 0;
       // Iterate over all possible split points
-      for (int k = j - 1; k >= i && resultsFound <= n; --k)
+      for (int k = j - 1; k >= i; --k) {
         // Iterate over all possible results of the left subexpression
         for (std::tuple<int, int, int, int> &left : dp[i][k]) {
           int leftResult = std::get<0>(left);
@@ -66,26 +73,33 @@ int main() {
           for (std::tuple<int, int, int, int> &right : dp[k + 1][j]) {
             int rightResult = std::get<0>(right);
             int result = operatorTable[leftResult][rightResult];
-            if (!used[result]) { // If this result has not been used yet
+            if (used[result]) { // If this result has not been used yet
+              continue;
+            }
             // Store the result, the split point and the left and right results used
-              dp[i][j].emplace_back(result, k, leftResult, rightResult);
-              used[result] = true; // Mark this result as used
-              ++resultsFound; // Increment the number of results found
+            dp[i][j].emplace_back(result, k, leftResult, rightResult);
+            used[result] = true; // Mark this result as used
+            ++resultsFound; // Increment the number of results found
+            // Stop if the target result is found and the length of the expression is m
+            if (result == targetResult && len == m) {
+              foundSolution = true;
+              break;
             }
             if (resultsFound >= n) break; // Stop if we have enough results
           }
-          if (resultsFound >= n) break; // Stop if we have enough results
+          if (resultsFound >= n || foundSolution) break; // Stop if we have enough results
         }
+        if (resultsFound >= n || foundSolution) break; // Stop if we have enough results
+      }
+      if (foundSolution) break; // Stop if we have found the target result
     }
+    if (foundSolution) break; // Stop if we have found the target result
+  }
 
-  for (std::tuple<int, int, int, int> &finalTuple : dp[0][m - 1])
-    if (std::get<0>(finalTuple) == targetResult) {
-      std::cout << 1 << std::endl;
-      // Calls recursive function to reconstruct the final expression
-      std::string parenthesizedSequence = reconstruct(0, m - 1, targetResult);
-      std::cout << parenthesizedSequence << std::endl;
-      return 0;
-    }
-  std::cout << 0 << std::endl; // Prints 0 to stdout if no solution is found
+  if (foundSolution)
+    // Calls the reconstruct function and prints the result to stdout
+    std::cout << 1 << std::endl << reconstruct(0, m - 1, targetResult) << std::endl;
+  else
+    std::cout << 0 << std::endl; // Prints 0 to stdout if no solution is found
   return 0;
 }
